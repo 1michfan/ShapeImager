4f7dc3ccd2562e6d769c186f133db46ba38683ba
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-26 21:13:23 -0400

    rename Form1 to ShapeListForm

--- ShapeImager.UI/My Project/Application.Designer.vb
+++ ShapeImager.UI/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ShapeImager.UI.Form1
+            Me.MainForm = Global.ShapeImager.UI.ShapeListForm
         End Sub
     End Class
 End Namespace
---
+++ ShapeImager.UI/ShapeListForm.vb
@@ -0,0 +1,6 @@
+﻿Public Class ShapeListForm
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim db As New Data.ShapeDbContect()
+        db.Circles.ToList()
+    End Sub
+End Class
