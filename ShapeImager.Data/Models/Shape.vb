95f531c8c0690ae9a235d4ccf8e2761ca23d5e35
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-26 20:27:56 -0400

    Create base Shape class

---
+++ ShapeImager.Data/Models/Shape.vb
@@ -0,0 +1,11 @@
+﻿
+Public MustInherit Class Shape
+    Public Property Id As Integer
+    'TODO store as int but convert to color?
+    Public Property CenterX As Decimal
+    Public Property CenterY As Decimal
+    Public Property Color As Integer
+
+    MustOverride Function GetArea() As Decimal
+    MustOverride Function GetPerimater() As Decimal()
+End Class
