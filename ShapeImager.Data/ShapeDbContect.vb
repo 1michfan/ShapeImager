40d4c115be8e783e25aeda97f765ee8100efb193
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-26 20:30:10 -0400

    add shapeDbContext class

---
+++ ShapeImager.Data/ShapeDbContect.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.Entity
+
+Public Class ShapeDbContect
+    Inherits DbContext
+
+    Public Sub New()
+        MyBase.New("ShapeDB")
+        Database.SetInitializer(New DropCreateDatabaseIfModelChanges(Of ShapeDbContect))
+        Database.Initialize(True)
+    End Sub
+
+    Public Overridable Property Shapes As DbSet(Of Shape)
+
+End Class
