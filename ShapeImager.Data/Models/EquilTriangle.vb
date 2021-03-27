537543a0845326dc890b4bf1c52be4786ce0a886
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-26 20:34:20 -0400

    add equiltriangle class

---
+++ ShapeImager.Data/Models/EquilTriangle.vb
@@ -0,0 +1,12 @@
+﻿Public Class EquilTriangle
+    Inherits Shape
+    Public Property SideLength As Decimal
+    Public Property Orientation As Decimal
+    Public Overrides Function GetArea() As Decimal
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function GetPerimater() As Decimal()
+        Throw New NotImplementedException()
+    End Function
+End Class
--- ShapeImager.Data/ShapeDbContect.vb
+++ ShapeImager.Data/ShapeDbContect.vb
@@ -9,6 +9,13 @@ Public Class ShapeDbContect
         Database.Initialize(True)
     End Sub
 
+    Protected Overrides Sub OnModelCreating(modelBuilder As DbModelBuilder)
+        MyBase.OnModelCreating(modelBuilder)
+        modelBuilder.Entity(Of Shape)().ToTable("Shapes")
+        modelBuilder.Entity(Of EquilTriangle)().ToTable("EquilTriangles")
+    End Sub
+
     Public Overridable Property Shapes As DbSet(Of Shape)
+    Public Overridable Property EquilTriangle As DbSet(Of EquilTriangle)
 
 End Class
