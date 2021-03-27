ab8226ae400770af99f536a099d382e2a6ebeca7
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-26 21:19:31 -0400

    add basic ui components

--- ShapeImager.UI/ShapeListForm.Designer.vb
+++ ShapeImager.UI/ShapeListForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ShapeListForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,81 @@ Partial Class ShapeListForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.tlpMain = New System.Windows.Forms.TableLayoutPanel()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.btnImportCsv = New System.Windows.Forms.Button()
+        Me.gvShape = New System.Windows.Forms.DataGridView()
+        Me.tlpMain.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        CType(Me.gvShape, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tlpMain
+        '
+        Me.tlpMain.ColumnCount = 2
+        Me.tlpMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpMain.Controls.Add(Me.FlowLayoutPanel1, 0, 0)
+        Me.tlpMain.Controls.Add(Me.gvShape, 0, 1)
+        Me.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tlpMain.Location = New System.Drawing.Point(0, 0)
+        Me.tlpMain.Name = "tlpMain"
+        Me.tlpMain.RowCount = 2
+        Me.tlpMain.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tlpMain.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tlpMain.Size = New System.Drawing.Size(800, 450)
+        Me.tlpMain.TabIndex = 0
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlowLayoutPanel1.AutoSize = True
+        Me.tlpMain.SetColumnSpan(Me.FlowLayoutPanel1, 2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnImportCsv)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(3, 3)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(794, 29)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'btnImportCsv
+        '
+        Me.btnImportCsv.Location = New System.Drawing.Point(3, 3)
+        Me.btnImportCsv.Name = "btnImportCsv"
+        Me.btnImportCsv.Size = New System.Drawing.Size(75, 23)
+        Me.btnImportCsv.TabIndex = 0
+        Me.btnImportCsv.Text = "Import CSV"
+        Me.btnImportCsv.UseVisualStyleBackColor = True
+        '
+        'gvShape
+        '
+        Me.gvShape.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gvShape.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gvShape.Location = New System.Drawing.Point(3, 38)
+        Me.gvShape.Name = "gvShape"
+        Me.gvShape.Size = New System.Drawing.Size(394, 409)
+        Me.gvShape.TabIndex = 1
+        '
+        'ShapeListForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.tlpMain)
+        Me.Name = "ShapeListForm"
+        Me.Text = "Shape List Form"
+        Me.tlpMain.ResumeLayout(False)
+        Me.tlpMain.PerformLayout()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        CType(Me.gvShape, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents tlpMain As TableLayoutPanel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents btnImportCsv As Button
+    Friend WithEvents gvShape As DataGridView
 End Class
