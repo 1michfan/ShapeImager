788fa4c6bc6e3b4a990ef1e00e4607d9381c9e97
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-30 15:55:36 -0400

    add ShapeSelectForm to allow user to select a shape form the list

---
+++ ShapeImager.UI/ShapeSelectForm.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ShapeSelectForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.FlowLayoutPanelButtons = New System.Windows.Forms.FlowLayoutPanel()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanelButtons
+        '
+        Me.FlowLayoutPanelButtons.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanelButtons.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanelButtons.Name = "FlowLayoutPanelButtons"
+        Me.FlowLayoutPanelButtons.Size = New System.Drawing.Size(412, 30)
+        Me.FlowLayoutPanelButtons.TabIndex = 0
+        '
+        'ShapeSelectForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 30)
+        Me.Controls.Add(Me.FlowLayoutPanelButtons)
+        Me.Name = "ShapeSelectForm"
+        Me.Text = "Select a Shape"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FlowLayoutPanelButtons As FlowLayoutPanel
+End Class
---
+++ ShapeImager.UI/ShapeSelectForm.vb
@@ -0,0 +1,22 @@
+﻿Imports ShapeImager.Data
+Public Class ShapeSelectForm
+    Public SelectedShape As Type
+    Dim _types As New List(Of Type) From {GetType(Circle), GetType(Ellipse), GetType(EquilTriangle), GetType(Polygon), GetType(Square)}
+
+    Public Sub New()
+        InitializeComponent()
+        For Each type In _types
+            Dim btn As New Button() With {
+                .Text = type.Name,
+                .Tag = type
+                }
+            AddHandler btn.Click, Sub() btn_ShapeSelected(type)
+            FlowLayoutPanelButtons.Controls.Add(btn)
+        Next
+    End Sub
+
+    Private Sub btn_ShapeSelected(type As Type)
+        SelectedShape = type
+        Close()
+    End Sub
+End Class
