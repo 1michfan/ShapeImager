d71f54b91e8349090d2c2a6959de53f30408a55a
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-31 16:40:02 -0400

    add ShapeImages.Test project and add CircleTests

---
+++ ShapeImager.Test/CircleTests.vb
@@ -0,0 +1,20 @@
+﻿Imports ShapeImager.Data
+
+<TestClass()> Public Class CircleTests
+
+    <TestMethod()>
+    Public Sub TestPerimeter()
+        Dim circle As New Circle(0, 0, 1)
+        Dim perimeter = Decimal.Round(circle.Perimeter, 2)
+        Dim radian As Decimal = 6.28
+        Assert.AreEqual(radian, perimeter)
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestRadius1EqualsRadius2()
+        Dim radius As Decimal = 1
+        Dim circle As New Circle(0, 0, radius)
+        Assert.AreEqual(circle.Radius1, circle.Radius2)
+    End Sub
+
+End Class
---
+++ ShapeImager.Test/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("ShapeImager.Test")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("ShapeImager.Test")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("81d0ab9e-ff7b-4a0d-af5d-f63fa7266782")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
