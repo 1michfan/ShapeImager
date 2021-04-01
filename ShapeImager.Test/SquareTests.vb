9e10d741af3fc53f95204902cac7dcd4b1adc2c2
Matt Oosterbaan <mattoosterbaan@gmail.com>
2021-03-31 20:24:50 -0400

    add more tests

---
+++ ShapeImager.Test/SquareTests.vb
@@ -0,0 +1,20 @@
+﻿Imports ShapeImager.Data
+
+<TestClass()>
+Public Class SquareTests
+
+    <TestMethod()>
+    Public Sub TestArea()
+        Dim sq As New Square(0, 0, sideLength:=1, 0)
+        Dim exp As Decimal = 1
+        Assert.AreEqual(exp, sq.Area)
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestPerimeter()
+        Dim sq As New Square(0, 0, sideLength:=1, 0)
+        Dim exp As Decimal = 4
+        Assert.AreEqual(exp, sq.Perimeter)
+    End Sub
+
+End Class
